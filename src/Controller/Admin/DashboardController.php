c2a82c0b7d5ce0946dc6d9fdd63f0c97add052c0
BlackSpel <mr.timur123@mail.ru>
2023-02-09 15:33:16 +0300

    Add user admin menu

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -27,8 +27,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-
-        //yield MenuItem::section('Users');
-        //yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
     }
 }
