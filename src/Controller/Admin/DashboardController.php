9cdb2232c4528043f9fd2f0d0c8805ca5d43ac19
BlackSpel <mr.timur123@mail.ru>
2023-02-10 11:03:04 +0300

    Change roles granted

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -27,6 +27,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
+        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class)->setPermission('ROLE_SUPER_ADMIN');
     }
 }
